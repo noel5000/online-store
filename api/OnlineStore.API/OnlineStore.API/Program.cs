dc9fe1f71e414ca268f02530cba18e02b0a01a96
Noel Jimenez <noelj@intralogicsolutions.com>
2024-08-19 15:26:10 -0400

    added identity

--- api/OnlineStore.API/OnlineStore.API/Program.cs
+++ api/OnlineStore.API/OnlineStore.API/Program.cs
@@ -1,12 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Common;
+using OnlineStore.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var appSettings = builder.Configuration.GetSection("AppSettings").Get<AppSettings>();
+builder.Services.AddSingleton(appSettings);
+builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:Main"]);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ api/OnlineStore.API/OnlineStore.Common/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineStore.Common
+{
+    public class AppSettings
+    {
+        public string Domain { get; set; }
+        public string TokenKey { get; set; }
+        public short TokenTime { get; set; }
+        public string ChainDbKeyToken { get; set; }
+    }
+}
