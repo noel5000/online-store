c6c3c42d523bd98aeb811c78e7a4977e60cb4281
Noel Jimenez <noelj@intralogicsolutions.com>
2024-09-26 19:39:57 -0400

    basic front end structure

---
+++ web/frontend/src/components/home.jsx
@@ -0,0 +1,25 @@
+
+import hero_logo from '../assets/img/hero-bg.jpg';
+
+
+import '../assets/vendor/bootstrap/js/bootstrap.bundle.min.js';
+import '../assets/vendor/php-email-form/validate.js';
+//import '../assets/js/main.js';
+
+export default function Home(){
+    return (<>
+        <section id="hero" className="hero section dark-background">
+    
+          <img src={hero_logo} alt="" data-aos="fade-in" />
+    
+          <div className="container">
+            <h2 data-aos="fade-up" data-aos-delay="100">Learning Today,<br />Leading Tomorrow</h2>
+            <p data-aos="fade-up" data-aos-delay="200">We are team of talented designers making websites with Bootstrap</p>
+            <div className="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
+              <a href="courses.html" className="btn-get-started">Get Started</a>
+            </div>
+          </div>
+    
+        </section>
+    </>)
+}
--- web/frontend/src/index.js
+++ web/frontend/src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './assets/css/main.css';
-import './assets/vendor/bootstrap/css/bootstrap.min.css';
-import './assets/vendor/bootstrap-icons/bootstrap-icons.css';
-import './assets/vendor/aos/aos.css';
-import './assets/vendor/glightbox/css/glightbox.min.css';
-import './assets/vendor/swiper/swiper-bundle.min.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
