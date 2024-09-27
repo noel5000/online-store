94680f22bfd5390a3c987871a3c1939f803cce4d
Noel Jimenez <noeljimenez.400@gmail.com>
2024-09-27 17:15:24 -0400

    Update menu.jsx

--- web/frontend/src/components/menu.jsx
+++ web/frontend/src/components/menu.jsx
@@ -2,20 +2,26 @@
 import '../assets/css/main.css';
 import '../assets/vendor/bootstrap/css/bootstrap.min.css';
 import '../assets/vendor/bootstrap-icons/bootstrap-icons.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 
 function MainMenu(){
+  const location = useLocation();
     const routes =[
         {route:"/", label:"Home", id:0},
         {route:"/store", label:"Store", id:1},
         {route:"/subscription", label:"Subscriptions", id:2},
         {route:"/contact", label:"Contact Us", id:3},
     ]
+    function isRouteActive(route) {
+      if (route === '/') {
+        return location.pathname === '/';
+      } else {
+        return location.pathname.startsWith(route);
+      }
+    }
 
     function renderMenu(){
-        const urlRoute = window.location.pathname.split('/');
-        let activeRoute = urlRoute.length < 2 ? '/' : urlRoute.filter(x=>x)[0];
         return <>
         <header id="header" className="header d-flex align-items-center sticky-top">
               <div className="container-fluid container-xl position-relative d-flex align-items-center">
@@ -26,7 +32,9 @@ function MainMenu(){
           
                 <nav id="navmenu" className="navmenu">
                   <ul>
-                    {routes.map(x=><li  key={x.id}> <Link to={x.route} className={x.route.replace('/','') == activeRoute? 'active': ''}  >{x.label}<br /></Link></li>)}
+                    {routes.map(x=>
+                    <li  key={x.id}> 
+                    <Link to={x.route} className={isRouteActive(x.route)? 'active': ''}  >{x.label}<br /></Link></li>)}
                   </ul>
                   <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
                 </nav>
